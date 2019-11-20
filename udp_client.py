e711dcb38c6fabd6382e3aae4caa5529a8ce3e7c
vadimdoga <vadim.doga1@gmail.com>
2019-11-20 18:02:19 +0200

    initial handshake connection using SYN SYN_ACK ACK

--- udp_client.py
+++ udp_client.py
@@ -6,14 +6,46 @@ msg_from_client = "hello from client"
 server_ip = "127.0.0.1"
 server_port = 8080
 server_address = (server_ip, server_port)
+buffer_size = 30000
+SYN = 4320
 #create dict with msg and cheksum
-json_to_send = protocol_library.create_json(msg_from_client)
+# json_to_send = protocol_library.create_json(msg_from_client)
 #udp connection
+client_header = protocol_library.Header(SYN, None).get_header_data()
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-  #send to server
-  sock.sendto(json_to_send, server_address)
-  #based on recv from server check if data is valid
-  recv_msg = protocol_library.client_verify_chksum(sock)
-  #print msg form server
-  msg_from_server = "Message from Server {}".format(recv_msg)
-  print(msg_from_server)
+  # #send to server
+  # sock.sendto(json_to_send, server_address)
+  # #based on recv from server check if data is valid
+  # recv_msg = protocol_library.client_verify_chksum(sock)
+  # #print msg form server
+  # msg_from_server = "Message from Server {}".format(recv_msg)
+  # print(msg_from_server)
+  client_header = pickle.dumps(client_header)
+  sock.sendto(client_header, server_address)
+
+  recv_from_server = sock.recvfrom(buffer_size)
+  client_header = recv_from_server[0]
+  client_header = pickle.loads(client_header)
+  print(client_header)
+  if client_header['ACK'] == (SYN + 1):
+    print("Connection established")
+    client_header = protocol_library.Header(client_header['SYN'], client_header['ACK'])
+    client_header.change_SYN_with_ACK()
+    client_header.increment_ACK()
+    client_header = client_header.get_header_data()
+    print(client_header)
+    client_header = pickle.dumps(client_header)
+    sock.sendto(client_header, server_address)
+# class Client:
+#   def __init__(self):
+#     self.client_state = CLOSED
+#     self.handshake()
+
+  #def handshake:
+
+  #def terminate
+
+  #update state(closed, listening)
+  #header with SYN,ACK
+
+  #state
--- udp_server.py
+++ udp_server.py
@@ -4,27 +4,47 @@ import protocol_library
 
 local_ip = "127.0.0.1"
 local_port = 8080
-
+buffer_size = 30000
 msg_from_server = b"hello from server"
+ACK = 5320
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
   sock.bind((local_ip, local_port))
   print("UDP server up and listening.")
 
   while True:
-    #recv dict from client
-    recv = protocol_library.recv_from_client(sock)
-    #verify the cheksum
-    verify_chksm = protocol_library.server_verify_chksm(sock, msg_from_server, recv['msg'], recv['chksm'], recv['address'])
+    recv_from_client = sock.recvfrom(buffer_size)
+    client_address = recv_from_client[1]
+    server_header = recv_from_client[0]
+    server_header = pickle.loads(server_header)
+    print(server_header)
+    server_header = protocol_library.Header(server_header['SYN'], ACK)
+    server_header.change_SYN_with_ACK()
+    server_header.increment_ACK()
+    server_header = server_header.get_header_data()
+    print(server_header)
+    server_header = pickle.dumps(server_header)
+    sock.sendto(server_header, client_address)
+
+    recv_from_client = sock.recvfrom(buffer_size)
+    server_header = recv_from_client[0]
+    server_header = pickle.loads(server_header)
+    if server_header['ACK'] == (ACK + 1):
+      print("Connection established")
+
+    # #recv dict from client
+    # recv = protocol_library.recv_from_client(sock)
+    # #verify the cheksum
+    # verify_chksm = protocol_library.server_verify_chksm(sock, msg_from_server, recv['msg'], recv['chksm'], recv['address'])
       
-    client_ip  = "Client IP Address:{}".format(recv['address'])
-    client_msg = "Message from Client:{}".format(recv['msg'])
+    # client_ip  = "Client IP Address:{}".format(recv['address'])
+    # client_msg = "Message from Client:{}".format(recv['msg'])
 
-    print(client_msg)
-    print(client_ip)
+    # print(client_msg)
+    # print(client_ip)
 
-    if verify_chksm:
-      break
+    # if verify_chksm:
+    #   break
 
 
     
