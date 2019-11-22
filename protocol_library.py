da52631e88fb328cb1a2148efd8b0bbf12e53f42
vadimdoga <vadim.doga1@gmail.com>
2019-11-22 11:51:23 +0200

    udp_client more abstract

--- protocol_library.py
+++ protocol_library.py
@@ -1,8 +1,10 @@
 import hashlib, pickle, json
 
 buffer_size = 30000
+server_address = ("127.0.0.1", 8080)
 error_msg = b"invalid_msg"
 server_ACK = 5320
+client_SYN = 4320
 
 def create_json(msg_from_client):
   encoded_msg_from_client = str.encode(msg_from_client)
@@ -99,3 +101,37 @@ def establish_connection(sock):
     verify_connection = verify_server_connection(server_header)
     if verify_connection:
       break
+
+#client handshake
+def initial_client_SYN():
+  client_header = Header(client_SYN, None).get_header_data()
+  client_header = pickle.dumps(client_header)
+  return client_header
+def process_client_header(client_header):
+  client_header = Header(client_header['SYN'], client_header['ACK'])
+  client_header.change_SYN_with_ACK()
+  client_header.increment_ACK()
+  client_header = client_header.get_header_data()
+  client_header = pickle.dumps(client_header)
+  return client_header
+def verify_client_connection(sock):
+  recv_from_server = sock.recvfrom(buffer_size)
+  client_header = recv_from_server[0]
+  client_header = pickle.loads(client_header)
+  if client_header['ACK'] == (client_SYN + 1):
+    client_header = process_client_header(client_header)
+    sock.sendto(client_header, server_address)
+    return True
+  else:
+    return False
+def handshake(sock):
+  client_header = initial_client_SYN()
+  sock.sendto(client_header, server_address)
+
+  verify_connection = verify_client_connection(sock)
+  if verify_connection:
+    print("Connection established")
+    return True
+  else:
+    print("Connection failed")
+    return False
