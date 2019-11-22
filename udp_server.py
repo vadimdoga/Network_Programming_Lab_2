8dfeaf63e6e03b70dbb058ecbbd60af901beeba6
vadimdoga <vadim.doga1@gmail.com>
2019-11-22 11:51:23 +0200

    implememnted three_way_handshake

--- udp_server.py
+++ udp_server.py
@@ -4,7 +4,6 @@ import protocol_library
 
 local_ip = "127.0.0.1"
 local_port = 8080
-buffer_size = 30000
 msg_from_server = b"hello from server"
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
@@ -13,22 +12,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
 
   protocol_library.establish_connection(sock)
 
-  # while True:
+  while True:
     
-
-    # #recv dict from client
-    # recv = protocol_library.recv_from_client(sock)
-    # #verify the cheksum
-    # verify_chksm = protocol_library.server_verify_chksm(sock, msg_from_server, recv['msg'], recv['chksm'], recv['address'])
+    #recv dict from client
+    recv = protocol_library.recv_from_client(sock)
+    #verify the cheksum
+    verify_chksm = protocol_library.server_verify_chksm(sock, msg_from_server, recv['msg'], recv['chksm'], recv['address'])
       
-    # client_ip  = "Client IP Address:{}".format(recv['address'])
-    # client_msg = "Message from Client:{}".format(recv['msg'])
+    client_ip  = "Client IP Address:{}".format(recv['address'])
+    client_msg = "Message from Client:{}".format(recv['msg'])
 
-    # print(client_msg)
-    # print(client_ip)
+    print(client_msg)
+    print(client_ip)
 
-    # if verify_chksm:
-    #   break
+    if verify_chksm:
+      break
 
 
 
