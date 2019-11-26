d05471536c8272a4e30c8f5852341a657a7eb504
vadimdoga <vadim.doga1@gmail.com>
2019-11-26 17:53:13 +0200

    code refactoring

--- udp_client.py
+++ udp_client.py
@@ -1,23 +1,6 @@
 import socket
 import library.protocol_library_client
 
-<<<<<<< HEAD
-msg_from_client = "hello from client"
-#create dict with msg and cheksum
-json_to_send = protocol_library.create_json(msg_from_client)
-#udp connection
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-  handshake = protocol_library.handshake(sock)
-  if handshake:
-    #send to server
-    sock.sendto(json_to_send, protocol_library.server_address)
-    #based on recv from server check if data is valid
-    recv_msg = protocol_library.client_verify_chksum(sock)
-    #print msg form server
-    print("Message from Server: ", recv_msg)
-  
-
-=======
 class Client:
   def __init__(self):
     # with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
@@ -34,4 +17,3 @@ if __name__ == "__main__":
 
   
 
->>>>>>> dev
--- udp_server.py
+++ udp_server.py
@@ -3,28 +3,6 @@ import library.protocol_library_server
 
 local_ip = "127.0.0.1"
 local_port = 8080
-<<<<<<< HEAD
-msg_from_server = b"Successful packet transmission"
-
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-  sock.bind((local_ip, local_port))
-  print("UDP server up and listening.")
-
-  protocol_library.establish_connection(sock)
-
-  while True:
-    
-    #recv dict from client
-    recv = protocol_library.recv_from_client(sock)
-    #verify the cheksum
-    verify_chksm = protocol_library.server_verify_chksm(sock, msg_from_server, recv['msg'], recv['chksm'], recv['address'])
-      
-    if verify_chksm:
-      print("Message from Client: ", recv['msg'])
-      break
-
-=======
->>>>>>> dev
 
 class Server:
   def __init__(self):
@@ -39,5 +17,3 @@ if __name__ == "__main__":
   server = Server()
   #wait recv from client
   library.protocol_library_server.wait_from_client(server)
-
-    
