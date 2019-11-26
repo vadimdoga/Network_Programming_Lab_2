a053b9deafb80b6131bf4e20bc5ca34ca5df27bf
vadimdoga <vadim.doga1@gmail.com>
2019-11-26 14:28:25 +0200

    protocol_library refactoring

--- udp_client.py
+++ udp_client.py
@@ -1,20 +1,23 @@
 import socket
-import protocol_library
+import protocol_library_client
+
+
+
 i = 0
 msg_from_client = "hello from client"
 #create dict with msg and cheksum
-json_to_send = protocol_library.create_json(msg_from_client, True)
+json_to_send = protocol_library_client.create_json(msg_from_client, True)
 #udp connection
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-  handshake = protocol_library.client_establish_connection(sock)
+  handshake = protocol_library_client.client_establish_connection(sock)
   if handshake:
     while True:
       if i == 5:
-        json_to_send = protocol_library.create_json(msg_from_client, False)
+        json_to_send = protocol_library_client.create_json(msg_from_client, False)
       #send to server
-      sock.sendto(json_to_send, protocol_library.server_address)
+      sock.sendto(json_to_send, protocol_library_client.SERVER_ADDRESS)
       #based on recv from server check if data is valid
-      recv_msg = protocol_library.client_verify_chksum(sock)
+      recv_msg = protocol_library_client.client_verify_chksum(sock)
       if recv_msg == "invalid_msg":
         print("Invalid msg, retransmitting message.")
       else:
@@ -22,7 +25,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         print("Message from Server: ", recv_msg)
         break
       i = i + 1
-    protocol_library.terminate_connection(sock)
+    protocol_library_client.terminate_connection(sock)
 
 
   
--- udp_server.py
+++ udp_server.py
@@ -1,5 +1,5 @@
 import socket 
-import protocol_library
+import protocol_library_server
 
 local_ip = "127.0.0.1"
 local_port = 8080
@@ -9,17 +9,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
   sock.bind((local_ip, local_port))
   print("UDP server up and listening.")
 
-  protocol_library.server_establish_connection(sock)
+  protocol_library_server.server_establish_connection(sock)
 
   while True:
     #recv dict from client
-    recv = protocol_library.server_receive(sock)
+    recv = protocol_library_server.server_receive(sock)
     if recv == 'fin':
       break
     else:
       #verify the cheksum
       #if chksm not valid retransmit
-      protocol_library.server_verify_chksm(sock, msg_from_server, recv['msg'], recv['chksm'], recv['address'])
+      protocol_library_server.server_verify_chksm(sock, msg_from_server, recv['msg'], recv['chksm'], recv['address'])
         
 
 
